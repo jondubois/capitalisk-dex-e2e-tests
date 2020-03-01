0f758352387fa0c69bc2979cc695769e7f2b8e0e
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2020-03-01 10:41:50 +0100

    Stress test

--- test/stress-test.js
+++ test/stress-test.js
@@ -2,25 +2,81 @@ const axios = require('axios');
 const ChainCrypto = require('../chain-crypto');
 const config = require('../config.json');
 
+const ORDER_ROUNDS = 15;
+
+// Full blocks
+const ORDERS_PER_ROUND = 25;
+
+const MIN_QUOTE_PRICE = .01;
+const MAX_QUOTE_PRICE = .1;
+const ORDER_AMOUNT_QUOTE = 1000000000;
+
+const ORDER_AMOUNT_BASE = 1000000000;
+
 let chainCrypto = new ChainCrypto();
 
 describe('Lisk DEX stress test', () => {
-  let latestBlockTimestamp;
 
-  beforeEach(async () => {
-    let {data: {data: blocks}} = await axios.get(`${config.baseChain.hostURL}/api/blocks?limit=1`);
-    latestBlockTimestamp = blocks[0].timestamp;
-  });
+  it('should send orders to the DEX address on the blockchain', async () => {
+    console.log('------- Start sending orders');
 
-  it('should ...', async () => {
-    let txn = chainCrypto.prepareTransaction({
-      amount: 110000000,
-      recipientId: config.baseChain.dexWallet.address,
-      timestamp: latestBlockTimestamp,
-      message: 'test 2'
-    }, config.baseChain.senderWallets[0].passphrase);
+    for (let i = 0; i < ORDER_ROUNDS; i++) {
+      let lastBlock = await getLastBlock();
+      let lastBlockTimestamp = lastBlock.timestamp;
 
-    let {data} = await axios.post(`${config.baseChain.hostURL}/api/transactions`, txn);
-    console.log('RESULT', data);
+      for (let j = 0; j < ORDERS_PER_ROUND; j++) {
+        let price = (Math.random() * (MAX_QUOTE_PRICE - MIN_QUOTE_PRICE) + MIN_QUOTE_PRICE).toFixed(2);
+        let orderIndex = (i * ORDERS_PER_ROUND) + j;
+        let askTxn = chainCrypto.prepareTransaction(
+          {
+            amount: ORDER_AMOUNT_QUOTE + orderIndex,
+            recipientId: config.quoteChain.dexWallet.address,
+            timestamp: lastBlockTimestamp + 1,
+            message: `${config.baseChain.symbol},limit,${price},${config.baseChain.userWallets[0].address}`
+          },
+          config.quoteChain.userWallets[0].passphrase
+        );
+        console.log(`Sending quote chain order ${orderIndex}:`, askTxn);
+        try {
+          let {data} = await axios.post(`${config.quoteChain.hostURL}/api/transactions`, askTxn);
+          console.log(`Quote chain order ${orderIndex} result:`, data);
+        } catch (error) {
+          console.error(`Quote chain failed to post order ${orderIndex}:`, error);
+        }
+      }
+
+      let bidTxn = chainCrypto.prepareTransaction(
+        {
+          amount: ORDER_AMOUNT_BASE + i,
+          recipientId: config.baseChain.dexWallet.address,
+          timestamp: lastBlockTimestamp + 1,
+          message: `${config.quoteChain.symbol},market,${config.quoteChain.userWallets[0].address}`
+        },
+        config.baseChain.userWallets[0].passphrase
+      );
+      console.log(`Sending base chain order ${i}:`, bidTxn);
+      try {
+        let {data} = await axios.post(`${config.baseChain.hostURL}/api/transactions`, bidTxn);
+        console.log(`Base chain order ${i} result:`, data);
+      } catch (error) {
+        console.error(`Base chain failed to post order ${i}:`, error);
+      }
+
+      await wait(10000);
+    }
+    console.log('------- Done sending orders');
   });
 });
+
+async function getLastBlock() {
+  let {data: {data: blocks}} = await axios.get(`${config.quoteChain.hostURL}/api/blocks?limit=1`);
+  return blocks[0];
+}
+
+function wait(duration) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, duration);
+  });
+}
